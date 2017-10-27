5829dfefce8ff6da9867a9e269b3ce7b15ee1579
AndriySavic <andriy1998412@gmail.com>
2017-10-27 13:37:28 +0300

    first commit

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,5 +5,5 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-#AdminUser.create!(email: 'andriy1998412@gmail.com', password: '75321998412', password_confirmation: '75321998412') if Rails.env.development?
-#AdminUser.create!(email: 'andriy1998412@gmail.com', password: '75321998412', password_confirmation: '75321998412') if Rails.env.production?
+AdminUser.create!(email: 'andriy1998412@gmail.com', password: '75321998412', password_confirmation: '75321998412') if Rails.env.development?
+AdminUser.create!(email: 'andriy1998412@gmail.com', password: '75321998412', password_confirmation: '75321998412') if Rails.env.production?
